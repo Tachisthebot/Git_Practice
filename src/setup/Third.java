0476f6723956d3254996fbb4c848dd372ba67f06
Casper <tachpro2@gmail.com>
2021-10-03 12:44:38 -0400

    Third file

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -4,6 +4,7 @@ public class Third {
     public static void main(String[] args) {
         //i want to reverse my word
         String str = "I love Java";
+        System.out.println(str);
 
     }
 }
