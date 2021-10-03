caf3f1b3881d438a5acd7108d4cbce39f38ef207
Casper <tachpro2@gmail.com>
2021-10-03 16:36:35 -0400

    wooden spoon

---
+++ src/tests/WoodenSpoon.java
@@ -0,0 +1,12 @@
+package tests;
+
+public class WoodenSpoon {
+
+        public static void main(String[] args) {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Wooden spoon");
+
+            }
+        }
+    }
+
