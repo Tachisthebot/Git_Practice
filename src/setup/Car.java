909bc626103f6fb14267a2dec5d86a2c4d6e60c0
Casper <tachpro2@gmail.com>
2021-10-03 15:48:17 -0400

    another one

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -1,16 +1,13 @@
 package setup;
 
-<<<<<<< HEAD
-public class Car {
-    public static void main(String[] args) {
-        System.out.println("My car is Tesla now");
-    }
-=======
+
+
 public class Car  {
 
     // this is my Tesla
+
     //i drive Tesla all the time
 
     //Tesla Tesla Tesla
->>>>>>> james
+
 }
